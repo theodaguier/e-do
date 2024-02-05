b00f54c2e13f6198b96e017dd04eedc9c2848862
theodaguier <theo.daguier@icloud.com>
2024-02-05 11:22:36 +0100

    merged

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
 import { Link, Tabs } from "expo-router";
 import { Pressable } from "react-native";
 import { Text } from "tamagui";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { DarkTheme } from "@react-navigation/native";
+
 import {
   Home,
   KanbanSquare,
@@ -10,27 +10,12 @@ import {
   ClipboardList,
   Menu,
 } from "@tamagui/lucide-icons";
+
 export default function TabLayout() {
-  const [session, setSession] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Utilisez une fonction asynchrone dans useEffect pour charger les données de manière asynchrone
-    const loadSession = async () => {
-      try {
-        const user = await AsyncStorage.getItem("userToken");
-        setSession(user);
-      } catch (error) {
-        console.error("Erreur lors du chargement de la session :", error);
-      }
-    };
-
-    loadSession();
-  }, []);
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "white",
+        tabBarActiveTintColor: DarkTheme.colors.primary,
       }}
     >
       <Tabs.Screen
@@ -38,13 +23,6 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => <Home size={24} color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       <Text className="px-4">Hello!</Text>
-          //     </Pressable>
-          //   </Link>
-          // ),
         }}
       />
       <Tabs.Screen
@@ -52,13 +30,6 @@ export default function TabLayout() {
         options={{
           title: "Sessions",
           tabBarIcon: ({ color }) => <ClipboardList size={24} color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       <Text className="px-4">Hello!</Text>
-          //     </Pressable>
-          //   </Link>
-          // ),
         }}
       />
       <Tabs.Screen
@@ -66,13 +37,6 @@ export default function TabLayout() {
         options={{
           title: "Studio",
           tabBarIcon: ({ color }) => <KanbanSquare size={24} color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       <Text className="px-4">Hello!</Text>
-          //     </Pressable>
-          //   </Link>
-          // ),
         }}
       />
       <Tabs.Screen
@@ -80,13 +44,6 @@ export default function TabLayout() {
         options={{
           title: "Clients",
           tabBarIcon: ({ color }) => <Users size={24} color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <Pressable>
-          //       <Text className="px-4">Hello!</Text>
-          //     </Pressable>
-          //   </Link>
-          // ),
         }}
       />
 
